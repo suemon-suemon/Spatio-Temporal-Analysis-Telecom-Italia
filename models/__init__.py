ba85278735bf63cd34e7c6238ccf9ab64eb790ba
zhangzw16 <thuzhangzw@outlook.com>
2022-04-18 23:36:52 -0400

    init STGCN-based model

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from models.STN import STN
 from models.Mvstgn import Mvstgn
 from models.Informer import Informer
 from models.TransformerE import TransformerEncoder
+from models.STTran import STTran
